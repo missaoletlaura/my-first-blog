efe4846304e4c594f3ea2ee2db3dbb6f32cb4a02
missaoletlaura <missaoletlaura@gmail.com>
2017-10-07 12:25:11 +0300

    My Django Girls app, first commit

---
+++ Pythonintro.py
@@ -0,0 +1,10 @@
+print(1 + 1)
+name = "Laura"
+print (name)
+kaupunki = "Tampere"
+print (kaupunki)
+print("Minun nimeni on" + " "+ name)
+print("Kotikaupunkini on" + " " + kaupunki)
+horoskooppi = "jousimies"
+print (horoskooppi)
+print("Horoskooppini on" + " "+ horoskooppi)
